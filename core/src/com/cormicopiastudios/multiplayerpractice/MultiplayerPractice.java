b13c553c80e499c0453a3bd88c2e51642922139d
cvhnilicka <cvhnilicka@wisc.edu>
2020-03-08 17:21:10 -0700

    starting to figure out some of the GWT and networking in java

--- core/src/com/cormicopiastudios/multiplayerpractice/MultiplayerPractice.java
+++ core/src/com/cormicopiastudios/multiplayerpractice/MultiplayerPractice.java
@@ -5,33 +5,62 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.github.czyzby.websocket.WebSocket;
+import com.github.czyzby.websocket.WebSocketAdapter;
+import com.github.czyzby.websocket.WebSockets;
+import com.github.czyzby.websocket.data.WebSocketCloseCode;
+import com.github.czyzby.websocket.net.ExtendedNet;
+import java.net.URI;
 
-import io.socket.client.Socket;
 
 public class MultiplayerPractice extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private SpriteBatch batch;
+	private WebSocket socket;
 
-	private Socket socket;
-	
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		// Note: you can also use WebSockets.newSocket() and WebSocket.toWebSocketUrl() methods.
+		socket = ExtendedNet.getNet().newWebSocket("127.0.0.1", 8000);
+//		socket = WebSockets.newSocket("http://127.0.0.1:8000");
+		socket.addListener(getListener());
+		socket.connect();
+	}
+
+	private static WebSocketAdapter getListener() {
+		return new WebSocketAdapter() {
+			@Override
+			public boolean onOpen(final WebSocket webSocket) {
+				Gdx.app.log("WS", "Connected!");
+				webSocket.send("Hello from client!");
+				return FULLY_HANDLED;
+			}
+
+			@Override
+			public boolean onClose(final WebSocket webSocket, final WebSocketCloseCode code, final String reason) {
+				Gdx.app.log("WS", "Disconnected - status: " + code + ", reason: " + reason);
+				return FULLY_HANDLED;
+			}
+
+			@Override
+			public boolean onMessage(final WebSocket webSocket, final String packet) {
+				Gdx.app.log("WS", "Got message: " + packet);
+				return FULLY_HANDLED;
+			}
+		};
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	public void render() {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
 		batch.end();
 	}
-	
+
 	@Override
-	public void dispose () {
+	public void dispose() {
+		WebSockets.closeGracefully(socket); // Null-safe closing method that catches and logs any exceptions.
 		batch.dispose();
-		img.dispose();
 	}
 }
--- desktop/src/com/cormicopiastudios/multiplayerpractice/desktop/DesktopLauncher.java
+++ desktop/src/com/cormicopiastudios/multiplayerpractice/desktop/DesktopLauncher.java
@@ -3,9 +3,11 @@ package com.cormicopiastudios.multiplayerpractice.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.cormicopiastudios.multiplayerpractice.MultiplayerPractice;
+import com.github.czyzby.websocket.CommonWebSockets;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+		CommonWebSockets.initiate();
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new MultiplayerPractice(), config);
 	}
